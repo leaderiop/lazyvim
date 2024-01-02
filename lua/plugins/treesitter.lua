c6eb00b4063cbd0cdf8b70b2f4a029dc27cfb528
Mohammad Almechkor <leaderiop@gmail.com>
2024-01-02 15:11:09 +0100

    feat: add rainbow parentheses powered by tree-sitter
    nvim-ts-rainbow2: https://github.com/HiPhish/nvim-ts-rainbow2

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2",
+    "windwp/nvim-ts-autotag",
+  },
   build = ":TSUpdate",
   config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
+    local configs = require('nvim-treesitter.configs')
+    configs.setup({
       ensure_installed = {"lua","javascript"},
       auto_install= true,
       highlight = { enable = true },
       indent = { enable = true },
+      rainbow = {
+        enable = true,
+        disable = { 'jsx', 'cpp' },
+        query = 'rainbow-parens',
+        strategy = require('ts-rainbow').strategy.global,
+      },
+      autotag = {
+        enable = true,
+      },
     })
   end
 }
